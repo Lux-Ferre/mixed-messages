1bc81e945ade084c414e7bb44e86dec4f1d8e8e3
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2023-02-18 20:46:31 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+The [adjective], [adjective] [noun] [verb] [preposition] the [adjective] [noun].
+eg "A quick brown fox jumps over the lazy dog."
+
+4 lists: adjective, noun, verb, preposition
+
+
+*/

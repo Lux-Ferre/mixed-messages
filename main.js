62f8f03fa2413fc179e8f85aa19b274a105b492c
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2023-02-18 21:16:17 +0000

    Functionality added

--- main.js
+++ main.js
@@ -7,23 +7,33 @@ eg "A quick brown fox jumps over the lazy dog."
 
 */
 
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+}
+
 function getAdjective(){
-    adjectiveList = ["quick", "lazy", "large", "small"]
+    adjectiveList = ["quick", "lazy", "large", "small", "smelly", "round", "sleepy", "energetic", "hyper", "lethargic", "witty", "clever", "dim", "bright"]
+    return adjectiveList.random()
 }
 
 function getColour(){
-    colourList = ["brown", "green", "purple", "red"]
+    colourList = ["brown", "green", "purple", "red", "yellow", "black", "white", "blue", "orange"]
+    return colourList.random()
 }
 
 function getNoun(){
     nounList = ["fox", "dog", "cat", "mouse", "elephant"]
+    return nounList.random()
 }
 
 function getVerb(){
     verbList = ["jumps", "leaps", "hops", "slinks", "skips", "flies"]
+    return verbList.random()
 }
 
 function getPreposition(){
-    prepositionList = ["over", "around", "under", "through"]
+    prepositionList = ["over", "around", "under", "with", "across", "behind", "for"]
+    return prepositionList.random()
 }
 
+console.log(`The ${getAdjective()}, ${getColour()} ${getNoun()} ${getVerb()} ${getPreposition()} the ${getAdjective()} ${getNoun()}.`)

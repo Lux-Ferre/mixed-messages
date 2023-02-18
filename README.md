4707326e02bb5eeed82698bf86d144df15990e92
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2023-02-18 21:00:44 +0000

    Lists formed & format narrowed slightly

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ CodeCademy Portfolio Project
 
 JavaScript program that generates random sentences of the format:
 
->The [adjective], [adjective] [noun] [verb] [preposition] the [adjective] [noun].
+>The [adjective], [colour] [noun] [verb] [preposition] the [adjective] [noun].
 
 e.g. "A quick brown fox jumps over the lazy dog."
--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 /*
-The [adjective], [adjective] [noun] [verb] [preposition] the [adjective] [noun].
+The [adjective], [colour] [noun] [verb] [preposition] the [adjective] [noun].
 eg "A quick brown fox jumps over the lazy dog."
 
-4 lists: adjective, noun, verb, preposition
+5 lists: adjective, colour, noun, verb, preposition
 
 
 */
+
+function getAdjective(){
+    adjectiveList = ["quick", "lazy", "large", "small"]
+}
+
+function getColour(){
+    colourList = ["brown", "green", "purple", "red"]
+}
+
+function getNoun(){
+    nounList = ["fox", "dog", "cat", "mouse", "elephant"]
+}
+
+function getVerb(){
+    verbList = ["jumps", "leaps", "hops", "slinks", "skips", "flies"]
+}
+
+function getPreposition(){
+    prepositionList = ["over", "around", "under", "through"]
+}
+
